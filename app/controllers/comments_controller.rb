fe202c8280b8b30e65c4f2395c6596dbdf9f1f5f
ruslan <budkingiv@mail.ru>
2010-08-12 22:40:01 -0700

    4 commit - added Comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+    if @comment.save
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(params[:comment])
+      render :action => 'show', :id => @comment
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def destroy
+    Comments.find(params[:id]).destroy
+    redirect_to :action => 'index'
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, :content
+  has_many :comments
 end
